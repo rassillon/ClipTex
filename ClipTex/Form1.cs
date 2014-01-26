7f503987b96d75b5f6871982d961bcba4c5c3329
rassillon <rassillondev@gmail.com>
2014-01-26 15:34:39 +0100

    Release v1.0
    Program fully functional

---
+++ ClipTex/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClipTex {
+    public partial class Form1 : Form {
+        DataSave ds = new DataSave();
+        string text = "";
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            Program.SaveTxtFile(ds.txtFilePath, Clipboard.GetText());
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            textBox1.Text = ds.txtFilePath;
+            if (Program.canWrite) {
+                button1.Enabled = true;
+            } else {
+                button1.Enabled = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            SaveFileDialog file = new SaveFileDialog();
+            file.InitialDirectory = System.Windows.Forms.Application.StartupPath;
+            file.Filter = "Text Files|*.txt";
+            if (file.ShowDialog() == DialogResult.OK) {
+                ds.txtFilePath = file.FileName;
+                Program.SaveTxtFile(ds.txtFilePath, text);
+                Program.WriteXML(ds);
+                textBox1.Text = ds.txtFilePath;
+            }
+
+            if (Program.canWrite) {
+                button1.Enabled = true;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e) {
+            Program.SaveTxtFile(ds.txtFilePath, text);
+            Program.WriteXML(ds);
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            Process.Start(ds.txtFilePath);
+        }
+
+    }
+}
---
+++ ClipTex/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace ClipTex {
+
+    static class Program {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+
+        static string DataSavePath = System.Windows.Forms.Application.StartupPath + @"\ClipTex.data";
+        public static bool canWrite = false;
+
+        [STAThread]
+        static void Main() {
+            DataSave ds = new DataSave();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        public static void WriteXML(DataSave ds) {
+            System.Xml.Serialization.XmlSerializer writer =
+                new System.Xml.Serialization.XmlSerializer(typeof(DataSave));
+
+            System.IO.StreamWriter file = new System.IO.StreamWriter(DataSavePath);
+            writer.Serialize(file, ds);
+            file.Close();
+        }
+
+        public static string ReadXML() {
+            string extract ="Please select a file.";
+            try {
+                XDocument data = XDocument.Load(DataSavePath);
+                extract = (String)data.Descendants().ToArray()[0];
+            } catch (FileNotFoundException e) {}
+            return extract;
+        }
+
+        public static void SaveTxtFile(String path, String text) {
+            if (text != null) {
+                try {
+                    File.WriteAllText(path, File.ReadAllText(path) + text + "\n");
+                } catch (FileNotFoundException e) {
+                    File.Create(path);
+                } catch (IOException e) {
+                    Console.Write("IOEcpetpion asdfasdf");
+                }
+            }
+        }
+    }
+
+    public class DataSave {
+        public string txtFilePath;
+
+        public DataSave() {
+            var temp = Program.ReadXML();
+            if (temp == null) {
+                txtFilePath = "File not created. Select one.";
+                Program.canWrite = false;
+            } else {
+                txtFilePath = temp;
+                Program.canWrite = true;
+            }
+        }
+    }
+}
